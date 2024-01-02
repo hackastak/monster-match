ad03280fde948c841562f1f10a5872e13d06f3e7
Austin Wiginton <ahwig93@gmail.com>
2024-01-02 08:23:40 -0600

    Add more card images and setup core game mechanic of flipping cards to match them and clearing out matched cards to win the game

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,88 +1,199 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react';
 
-import shortid from 'shortid'
-import Card from './components/Card'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import monsterMatchLogo from '/monster-match.png'
-import blueThunder from '/blue-thunder.png'
-import graveDigger from '/grave-digger.png'
-import maxD from '/max-d.png'
-import mmDalmation from '/mm-dalmation.png'
-import whiplash from '/whiplash.png'
-import zombie from '/zombie.png'
-import kraken from '/kraken.png'
+import shortid from 'shortid';
+import Card from './components/Card';
+import monsterMatchLogo from '/monster-match.png';
+import megaladon from '/megaladon.png';
+import blueThunder from '/blue-thunder.png';
+import graveDigger from '/grave-digger.png';
+import maxD from '/max-d.png';
+import mmDalmation from '/mm-dalmation.png';
+import whiplash from '/whiplash.png';
+import zombie from '/zombie.png';
+import kraken from '/kraken.png';
 
-import './App.css'
+import './App.css';
 
-let cards = [
+let uniqueCards = [
   {
-    image: monsterMatchLogo,
-    name: 'Monster Match'
+    image: megaladon,
+    name: 'Megaladon',
   },
   {
     image: blueThunder,
-    name: 'Blue Thunder'
-  }, 
+    name: 'Blue Thunder',
+  },
   {
     image: graveDigger,
-    name: 'Grave Digger'
-  }, 
+    name: 'Grave Digger',
+  },
   {
     image: maxD,
-    name: 'Max D'
+    name: 'Max D',
   },
   {
     image: mmDalmation,
-    name: 'Monster Mutt Dalmation'
+    name: 'Monster Mutt Dalmation',
   },
   {
     image: whiplash,
-    name: 'Whiplash'
+    name: 'Whiplash',
   },
   {
     image: zombie,
-    name: 'Zombie'
+    name: 'Zombie',
   },
   {
     image: kraken,
-    name: 'Kraken'
+    name: 'Kraken',
   },
-]
-const dupCards = cards.concat(cards)
+];
 
 const shuffle = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i +1));
+    const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
   }
   return array;
-}
+};
 
 const App = () => {
-  const [playing, setPlaying] = useState(false)
-  const [gameCards, setGameCards] = useState([])
+  const [cards, setCards] = useState(() => {
+    shuffle(uniqueCards.concat(uniqueCards));
+  });
+  const [openCards, setOpenCards] = useState([]);
+  const [clearedCards, setClearedCards] = useState([]);
+  const [shouldDisableAllCards, setShouldDisableAllCards] = useState(false);
+  // const [showModal, setShowModal] = useState(false);
+  const [playing, setPlaying] = useState(false);
+  const [winnerCircle, setWinnerCircle] = useState(false);
   const playGame = () => {
-    setPlaying(true)
-    setGameCards(shuffle(dupCards))
-  }
+    setPlaying(true);
+    setCards(shuffle(uniqueCards.concat(uniqueCards)));
+  };
+  const timeout = useRef(null);
+
+  const checkCompletion = () => {
+    if (Object.keys(clearedCards).length === uniqueCards.length) {
+      console.log('YOU WON!!!');
+      setPlaying(false);
+      setWinnerCircle(true);
+    }
+  };
+
+  const handleCardClick = (index) => {
+    if (openCards.length === 1) {
+      setOpenCards((prev) => [...prev, index]);
+      disable();
+    } else {
+      clearTimeout(timeout.current);
+      setOpenCards([index]);
+    }
+  };
+
+  const disable = () => {
+    setShouldDisableAllCards(true);
+  };
+
+  const enable = () => {
+    setShouldDisableAllCards(false);
+  };
+
+  const checkIsInactive = (card) => {
+    return Boolean(clearedCards[card.name]);
+  };
+
+  const checkIsFlipped = (index) => {
+    return openCards.includes(index);
+  };
+
+  const handleRestart = () => {
+    setClearedCards({});
+    setOpenCards([]);
+    setShouldDisableAllCards(false);
+    setWinnerCircle(false);
+    setPlaying(true);
+    setCards(shuffle(uniqueCards.concat(uniqueCards)));
+  };
+
+  useEffect(() => {
+    let timeout = null;
+
+    const evaluateMatch = () => {
+      const [first, second] = openCards;
+      enable();
+      if (cards[first].name === cards[second].name) {
+        console.log('Yeah they match');
+        setClearedCards((prev) => ({ ...prev, [cards[first].name]: true }));
+        setOpenCards([]);
+        return;
+      }
+      console.log(clearedCards);
+      timeout.current = setTimeout(() => {
+        setOpenCards([]);
+      }, 500);
+    };
+    console.log(clearedCards);
+    if (openCards.length === 2) {
+      timeout = setTimeout(evaluateMatch, 500);
+    }
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [openCards]);
+
+  useEffect(() => {
+    checkCompletion();
+  }, [clearedCards]);
 
   return (
     <>
       <h1>Monster Match</h1>
-      {(playing) ? (  
-        <div className="game-board-container">
-          {gameCards.map(function(card) {
-            return (
-              <Card key={shortid.generate()} image={card.image} name={card.name} />
-            )
-          })}
-        </div>
+      {playing ? (
+        <>
+          <div className='game-board-container'>
+            {cards.map((card, index) => {
+              return (
+                <Card
+                  key={index}
+                  card={card}
+                  index={index}
+                  isDisabled={shouldDisableAllCards}
+                  isInactive={checkIsInactive(card)}
+                  isFlipped={checkIsFlipped(index)}
+                  onClick={handleCardClick}
+                />
+              );
+            })}
+          </div>
+          <button className='btn-main' onClick={handleRestart}>
+            Restart
+          </button>
+        </>
       ) : (
-        <button className="btn-main" onClick={playGame}>Play Game</button>
+        <button className='btn-main' onClick={playGame}>
+          Play Game
+        </button>
+      )}
+      {winnerCircle && (
+        <>
+          <div className='giffy-embed'>
+            <iframe
+              src='https://giphy.com/embed/POpr7nTrhogeI'
+              width='100%'
+              height='100%'
+              allowFullScreen
+            ></iframe>
+          </div>
+          <p>
+            <a href='https://giphy.com/gifs/monsterjam-monster-jam-grave-digger-dennis-anderson-POpr7nTrhogeI'>
+              via GIPHY
+            </a>
+          </p>
+        </>
       )}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,37 +1,42 @@
-import {useState} from 'react'
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import monsterMatchLogo from '/monster-match.png';
 
-import "./component-styles.css"
-
-
+import './component-styles.css';
 
 const Card = (props) => {
-  const [flipped, setFlipped] = useState(false);
+  const { isFlipped, isDisabled, isInactive, onClick } = props;
 
-  const flipCard = () => {
-    setFlipped(!flipped);
-  }
+  const handleClick = () => {
+    !isFlipped && !isDisabled && onClick(props.index);
+  };
 
   return (
-    <div className="card-container">
-      <div className="card" onClick={flipCard}>
-        <div className="image-container" name={props.name}>
-          {flipped ? (
-              <img className="card-image" src={props.image} />
-            ) : (
-              <p>Monster Match</p>
-            )
-          }
+    <div className='card-container'>
+      <div
+        className={'card ' + (isInactive && 'cleared')}
+        onClick={handleClick}
+      >
+        <div className='image-container' name={props.card.name}>
+          {isFlipped ? (
+            <img
+              className='card-image card-front-face'
+              src={props.card.image}
+            />
+          ) : (
+            <img className='card-image card-back-face' src={monsterMatchLogo} />
+          )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 Card.propTypes = {
-  name: PropTypes.string,
-  image: PropTypes.string,
-}
+  card: PropTypes.object,
+  index: PropTypes.number,
+  onClick: PropTypes.func,
+  isFlipped: PropTypes.bool,
+  isDisabled: PropTypes.bool,
+};
 
-export default Card
-
+export default Card;
